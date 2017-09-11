1551fffdec2cdac42fdb1b27ad87f4e32287a871
nazzy <525291056@qq.com>
2017-09-12 00:23:27 +0800

    讲师列表查看功能完成

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -1,2 +1,25 @@
 require("../common/header.js");
 require("../common/aside.js");
+
+/* 请求教师列表的ajax */
+$.get("/v6/teacher", function(data) {
+    if (data.code == 200) {
+        $(".table_teacher_list tbody").append(template("teacher_list_tpl", data.result));
+    }
+})
+
+/* 请求查看教师信息的ajax */
+$(document).on("click", ".btn_look", function() {
+    var tc_id = $(this).attr("data-id");
+    $.ajax({
+        url: "/v6/teacher/view",
+        type: "get",
+        data: { tc_id: tc_id },
+        delegation: true,
+        success: function(data) {
+            if (data.code == 200) {
+                $("#panel_list").html(template("teacher_ifo_tpl", data.result));
+            }
+        }
+    })
+})
